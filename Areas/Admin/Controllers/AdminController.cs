a699cd1b424e2979f17397beba9bd1205dcb39e5
cuong333 <dangcuong9331@gmail.com>
2024-10-14 07:54:35 +0700

    admin layout

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoDLC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/admin")]
+    public class AdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
