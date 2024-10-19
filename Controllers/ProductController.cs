d12a07de7b4e0ffc2de68091b275c9e8b8bc500e
cuong333 <dangcuong9331@gmail.com>
2024-10-19 21:56:47 +0700

    Product images

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using DemoDLC.Models;
-namespace DemoDLC.Controllers
-{
-    public class ProductController : Controller
-    {
-        public IActionResult Index()
-        {
-            var listProduct = new List<Product>();
-            return View();
-        }
-        public IActionResult ProductDetail()
-        {
-            return View("~/Views/Checkout/ProductDetail.cshtml");
-        }
-    }
-}
+﻿//using Microsoft.AspNetCore.Mvc;
+//using DemoDLC.Models;
+//namespace DemoDLC.Controllers
+//{
+//    public class ProductController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            var listProduct = new List<Product>();
+//            return View();
+//        }
+//        public IActionResult ProductDetail()
+//        {
+//            return View("~/Views/Checkout/ProductDetail.cshtml");
+//        }
+//    }
+//}
--- Data/DemoDlcContext.cs
+++ Data/DemoDlcContext.cs
@@ -39,7 +39,6 @@ public partial class DemoDlcContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=CUONGSEVEN\\MSSQLSERVER01;Initial Catalog=DemoDLC;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
